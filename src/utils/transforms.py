f59c8f71f0eb992e8e26533fd8eddc86efa30de6
zch <68337357+flateon@users.noreply.github.com>
2023-10-15 12:28:11 +0800

    finish Part2 Transform

--- src/utils/transforms.py
+++ src/utils/transforms.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Transform:
     """
     Preprocess time series
@@ -19,7 +22,7 @@ class Transform:
 
 
 class IdentityTransform(Transform):
-    def __init__(self, args):
+    def __init__(self):
         pass
 
     def transform(self, data):
@@ -28,4 +31,99 @@ class IdentityTransform(Transform):
     def inverse_transform(self, data):
         return data
 
-# TODO: add other transforms
+
+class LinearTransform(Transform):
+    def __init__(self, weight=None, bias=None):
+        self.weight = weight
+        self.bias = bias
+
+    def set_weight_bias(self, data):
+        raise NotImplementedError
+
+    def transform(self, data):
+        if self.weight is None or self.bias is None:
+            self.set_weight_bias(data)
+        return data * self.weight + self.bias
+
+    def inverse_transform(self, data):
+        if self.weight is None or self.bias is None:
+            raise ValueError('Weight or bias is None')
+        else:
+            return (data - self.bias) / self.weight
+
+
+class NormalizationTransform(LinearTransform):
+    def __init__(self):
+        super().__init__()
+
+    def set_weight_bias(self, data):
+        y_max = np.max(data, axis=1, keepdims=True)
+        y_min = np.min(data, axis=1, keepdims=True)
+        self.weight = 1 / (y_max - y_min + 1e-8)
+        self.bias = -y_min * self.weight
+
+
+class StandardizationTransform(LinearTransform):
+    def __init__(self):
+        super().__init__()
+
+    def set_weight_bias(self, data):
+        mean = np.mean(data, axis=1, keepdims=True)
+        std = np.std(data, axis=1, keepdims=True)
+        self.weight = 1 / (std + 1e-8)
+        self.bias = -mean * self.weight
+
+
+class MeanNormalizationTransform(LinearTransform):
+    def __init__(self):
+        super().__init__()
+
+    def set_weight_bias(self, data):
+        mean = np.mean(data, axis=1, keepdims=True)
+        y_max = np.max(data, axis=1, keepdims=True)
+        y_min = np.min(data, axis=1, keepdims=True)
+        self.weight = 1 / (y_max - y_min + 1e-8)
+        self.bias = -mean * self.weight
+
+
+class YeoJohnsonTransform(Transform):
+    def __init__(self, lamda):
+        self.lamda = lamda
+
+    def transform(self, data):
+        result = np.zeros_like(data)
+        lamda = self.lamda
+        pos_mask = data >= 0
+        neg_mask = ~pos_mask
+
+        if np.isclose(lamda, 0):
+            result[pos_mask] = np.log1p(data[pos_mask])
+        else:
+            result[pos_mask] = (np.power(data[pos_mask] + 1, lamda) - 1) / lamda
+
+        if np.isclose(lamda, 2):
+            result[neg_mask] = -np.log1p(-data[neg_mask])
+        else:
+            result[neg_mask] = -(np.power(-data[neg_mask] + 1, 2 - lamda) - 1) / (2 - lamda)
+        return result
+
+    def inverse_transform(self, data):
+        result = np.zeros_like(data)
+        lamda = self.lamda
+        pos_mask = data >= 0
+        neg_mask = ~pos_mask
+
+        if np.isclose(lamda, 0):
+            result[pos_mask] = np.exp(data[pos_mask]) - 1
+        else:
+            result[pos_mask] = np.power(data[pos_mask] * lamda + 1, 1 / lamda) - 1
+
+        if np.isclose(lamda, 2):
+            result[neg_mask] = 1 - np.exp(-data[neg_mask])
+        else:
+            result[neg_mask] = 1 - np.power(-(2 - lamda) * data[neg_mask] + 1, 1 / (2 - lamda))
+        return result
+
+
+class BoxCoxTransform(YeoJohnsonTransform):
+    pass
---
+++ tests/test_transforms.py
@@ -0,0 +1,78 @@
+import unittest
+
+import numpy as np
+from src.utils.transforms import *
+
+
+class TestTransforms(unittest.TestCase):
+
+    def setUp(self):
+        # element: [-100, 100]
+        self.data = [np.random.random((1, 1000, 10)) * 200 - 100,
+                     np.random.random((10, 1000, 10)) * 200 - 100,
+                     np.random.random((10, 1000)) * 200 - 100]
+        self.lamda = [-1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5]
+
+    def test_identity_transform(self):
+        for data in self.data:
+            identity_transform = IdentityTransform()
+
+            transformed_data = identity_transform.transform(data)
+            self.assertTrue(np.allclose(transformed_data, data))
+
+            inv_transformed_data = identity_transform.inverse_transform(data)
+            self.assertTrue(np.allclose(inv_transformed_data, data))
+
+    def test_normalization_transform(self):
+        for data in self.data:
+            normalization_transform = NormalizationTransform()
+
+            transformed_data = normalization_transform.transform(data)
+            y_max = np.max(data, axis=1, keepdims=True)
+            y_min = np.min(data, axis=1, keepdims=True)
+            expected_transformed_data = (data - y_min) / (y_max - y_min + 1e-8)
+            self.assertTrue(np.allclose(transformed_data, expected_transformed_data))
+
+            inv_transformed_data = normalization_transform.inverse_transform(transformed_data)
+            self.assertTrue(np.allclose(inv_transformed_data, data))
+
+    def test_standardization_transform(self):
+        for data in self.data:
+            standardization_transform = StandardizationTransform()
+
+            transformed_data = standardization_transform.transform(data)
+            mean = np.mean(data, axis=1, keepdims=True)
+            std = np.std(data, axis=1, keepdims=True)
+            expected_transformed_data = (data - mean) / (std + 1e-8)
+            self.assertTrue(np.allclose(transformed_data, expected_transformed_data))
+
+            inv_transformed_data = standardization_transform.inverse_transform(transformed_data)
+            self.assertTrue(np.allclose(inv_transformed_data, data))
+
+    def test_mean_normalization_transform(self):
+        for data in self.data:
+            mean_normalization_transform = MeanNormalizationTransform()
+
+            transformed_data = mean_normalization_transform.transform(data)
+            mean = np.mean(data, axis=1, keepdims=True)
+            y_max = np.max(data, axis=1, keepdims=True)
+            y_min = np.min(data, axis=1, keepdims=True)
+            expected_transformed_data = (data - mean) / (y_max - y_min + 1e-8)
+            self.assertTrue(np.allclose(transformed_data, expected_transformed_data))
+
+            inv_transformed_data = mean_normalization_transform.inverse_transform(transformed_data)
+            self.assertTrue(np.allclose(inv_transformed_data, data))
+
+    def test_box_cox_transform(self):
+        for data in self.data:
+            for lamda in self.lamda:
+                box_cox_transform = BoxCoxTransform(lamda)
+
+                transformed_data = box_cox_transform.transform(data)
+                inverse_transformed_data = box_cox_transform.inverse_transform(transformed_data)
+
+                self.assertTrue(np.allclose(data, inverse_transformed_data))
+
+
+if __name__ == '__main__':
+    unittest.main()
