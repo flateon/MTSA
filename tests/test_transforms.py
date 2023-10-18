96d0e536b0acb070dbc176565477d1435dbf5346
zch <68337357+flateon@users.noreply.github.com>
2023-10-18 17:05:12 +0800

    add transforms unittest

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -9,10 +9,19 @@ class TestTransforms(unittest.TestCase):
 
     def setUp(self):
         # element: [-100, 100]
-        self.data = [np.random.random((1, 1000, 10)) * 200 - 100,
-                     np.random.random((10, 1000, 10)) * 200 - 100,
-                     np.random.random((10, 1000)) * 200 - 100]
+        self.data = [
+            np.random.random((1, 1000, 10)) * 20 - 10,
+            np.random.random((10, 100, 10)) * 20 - 30,
+            np.random.random((1, 1000, 10)) * 0.2 - 0.1,
+            np.random.random((1, 1000, 10)) * 2e-9 - 1e-9,
+            np.random.random((1, 1000, 10)) * 2e9 - 1e9,
+        ]
         self.lamda = [-1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5]
+
+    def test_transform(self):
+        t = Transform()
+        self.assertRaises(NotImplementedError, t.transform, None)
+        self.assertRaises(NotImplementedError, t.inverse_transform, None)
 
     def test_identity_transform(self):
         for data in self.data:
@@ -23,6 +32,29 @@ class TestTransforms(unittest.TestCase):
 
             inv_transformed_data = identity_transform.inverse_transform(data)
             self.assertTrue(np.allclose(inv_transformed_data, data))
+
+    def test_linear_transform(self):
+        t = LinearTransform()
+        self.assertRaises(NotImplementedError, t.set_weight_bias, None)
+        self.assertRaises(ValueError, t.inverse_transform, None)
+
+        for data in self.data:
+            weight_like = np.random.random((data.shape[0], 1, data.shape[2]))
+            linear_transform = LinearTransform(weight=weight_like, bias=weight_like)
+
+            transformed_data = linear_transform.transform(data)
+            expected_transformed_data = data * linear_transform.weight + linear_transform.bias
+            self.assertTrue(np.allclose(transformed_data, expected_transformed_data))
+
+            inv_transformed_data = linear_transform.inverse_transform(transformed_data)
+            self.assertTrue(np.allclose(inv_transformed_data, data))
+
+            target = data[..., -1]
+            transformed_target = linear_transform.transform(target, target_only=True)
+            self.assertTrue(np.allclose(transformed_target, transformed_data[..., -1]))
+
+            inv_transformed_target = linear_transform.inverse_transform(transformed_target, target_only=True)
+            self.assertTrue(np.allclose(inv_transformed_target, target))
 
     def test_normalization_transform(self):
         for data in self.data:
@@ -37,6 +69,13 @@ class TestTransforms(unittest.TestCase):
             inv_transformed_data = normalization_transform.inverse_transform(transformed_data)
             self.assertTrue(np.allclose(inv_transformed_data, data))
 
+            target = data[..., -1]
+            transformed_target = normalization_transform.transform(target, target_only=True)
+            self.assertTrue(np.allclose(transformed_target, transformed_data[..., -1]))
+
+            inv_transformed_target = normalization_transform.inverse_transform(transformed_target, target_only=True)
+            self.assertTrue(np.allclose(inv_transformed_target, target))
+
     def test_standardization_transform(self):
         for data in self.data:
             standardization_transform = StandardizationTransform()
@@ -49,6 +88,13 @@ class TestTransforms(unittest.TestCase):
 
             inv_transformed_data = standardization_transform.inverse_transform(transformed_data)
             self.assertTrue(np.allclose(inv_transformed_data, data))
+
+            target = data[..., -1]
+            transformed_target = standardization_transform.transform(target, target_only=True)
+            self.assertTrue(np.allclose(transformed_target, transformed_data[..., -1]))
+
+            inv_transformed_target = standardization_transform.inverse_transform(transformed_target, target_only=True)
+            self.assertTrue(np.allclose(inv_transformed_target, target))
 
     def test_mean_normalization_transform(self):
         for data in self.data:
@@ -64,6 +110,14 @@ class TestTransforms(unittest.TestCase):
             inv_transformed_data = mean_normalization_transform.inverse_transform(transformed_data)
             self.assertTrue(np.allclose(inv_transformed_data, data))
 
+            target = data[..., -1]
+            transformed_target = mean_normalization_transform.transform(target, target_only=True)
+            self.assertTrue(np.allclose(transformed_target, transformed_data[..., -1]))
+
+            inv_transformed_target = mean_normalization_transform.inverse_transform(transformed_target,
+                                                                                    target_only=True)
+            self.assertTrue(np.allclose(inv_transformed_target, target))
+
     def test_box_cox_transform(self):
         for data in self.data:
             for lamda in self.lamda:
@@ -73,3 +127,10 @@ class TestTransforms(unittest.TestCase):
                 inverse_transformed_data = box_cox_transform.inverse_transform(transformed_data)
 
                 self.assertTrue(np.allclose(data, inverse_transformed_data))
+
+                target = data[..., -1]
+                transformed_target = box_cox_transform.transform(target, target_only=True)
+                self.assertTrue(np.allclose(transformed_target, transformed_data[..., -1]))
+
+                inv_transformed_target = box_cox_transform.inverse_transform(transformed_target, target_only=True)
+                self.assertTrue(np.allclose(inv_transformed_target, target))
