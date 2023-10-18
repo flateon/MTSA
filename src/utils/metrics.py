f2175d7da3407bbf018c043b40a2003d076ff502
zch <68337357+flateon@users.noreply.github.com>
2023-10-18 16:29:41 +0800

    fix metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+np.seterr(divide='ignore')
+
 
 def mse(predict, target):
     return np.mean((target - predict) ** 2)
@@ -10,14 +12,16 @@ def mae(predict, target):
 
 
 def mape(predict, target):
-    return 100 * np.mean(np.abs((target - predict) / target))
+    ape = np.abs((target - predict) / target)
+    # fix divide by zero
+    return 100 * np.mean(np.nan_to_num(ape, copy=False, nan=0.0, posinf=0.0, neginf=0.0))
 
 
 def smape(predict, target):
-    return 200 * np.mean(np.abs(target - predict) / (np.abs(target) + np.abs(predict)))
+    sape = np.abs(target - predict) / (np.abs(target) + np.abs(predict))
+    return 200 * np.mean(np.nan_to_num(sape, copy=False, nan=0.0, posinf=0.0, neginf=0.0))
 
 
 def mase(predict, target, m: int):
-    assert len(target) > m
-    assert isinstance(m, int)
-    return np.mean(np.abs(target - predict)) / np.mean(np.abs(target[m:] - target[:-m]))
+    mase_ = np.mean(np.abs(target - predict)) / np.mean(np.abs(target[m:] - target[:-m]))
+    return np.nan_to_num(mase_, copy=False, nan=0, posinf=0.0, neginf=0.0)
