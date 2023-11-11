3352bad1b42dda7a667dd3026d4df81e0877783f
zch <68337357+flateon@users.noreply.github.com>
2023-11-11 11:56:15 +0800

    improve decomposition

--- src/utils/decomposition.py
+++ src/utils/decomposition.py
@@ -15,21 +15,13 @@ def moving_average(X, period=25):
     X = X.transpose((0, 2, 1)).reshape((-1, timestamp))
 
     trend_a = np.zeros_like(X)
-    seasonal_a = np.zeros_like(X)
     filter = np.repeat(1.0 / period, period)
 
     for i, x in enumerate(X):
-        x_pad = np.pad(x, ((period - 1) // 2, (period) // 2), mode='wrap')
-        trend = np.convolve(x_pad, filter, mode='valid')
-
-        seasonal = x - trend
-        #
-        # period_row = detrended[:timestamp // period * period].reshape((-1, period))
-        # period_averages = np.mean(period_row, axis=0)
-        #
-        # seasonal = np.tile(period_averages, timestamp // period + 1)[:timestamp]
-
-        trend_a[i], seasonal_a[i] = trend, seasonal
+        # x_pad = np.pad(x, ((period - 1) // 2, (period) // 2), mode='wrap')
+        trend = np.convolve(x, filter, mode='same')
+        trend_a[i] = trend
+    seasonal_a = X - trend_a
     trend_a = trend_a.reshape((n_samples, n_channels, timestamp)).transpose((0, 2, 1))
     seasonal_a = seasonal_a.reshape((n_samples, n_channels, timestamp)).transpose((0, 2, 1))
     return trend_a, seasonal_a
