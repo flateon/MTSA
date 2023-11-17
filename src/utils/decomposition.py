2c133422384b1cc5f08e229244c68c0d0900401c
zch <68337357+flateon@users.noreply.github.com>
2023-11-17 21:12:32 +0800

    reimplement moving average

--- src/utils/decomposition.py
+++ src/utils/decomposition.py
@@ -1,28 +1,23 @@
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 
-def moving_average(X, period=25):
+def moving_average(x, period=25):
     """
     Moving Average Algorithm
     Args:
-        X (numpy.ndarray): Input time series data (n_samples, timestamp, n_channels)
+        x (numpy.ndarray): Input time series data (n_samples, timestamp, n_channels)
         period (int): Seasonal period
     Returns:
         trend (numpy.ndarray): Trend component
         seasonal (numpy.ndarray): Seasonal component
     """
-    n_samples, timestamp, n_channels = X.shape
-    X = X.transpose((0, 2, 1)).reshape((-1, timestamp))
-
-    trend = np.zeros_like(X)
-    filter = np.repeat(1.0 / period, period)
-
-    for i, x in enumerate(X):
-        # x_pad = np.pad(x, ((period - 1) // 2, (period) // 2), mode='wrap')
-        trend[i] = np.convolve(x, filter, mode='same')
-    seasonal = X - trend
-    trend = trend.reshape((n_samples, n_channels, timestamp)).transpose((0, 2, 1))
-    seasonal = seasonal.reshape((n_samples, n_channels, timestamp)).transpose((0, 2, 1))
+    pad_width = np.array(((0, 0),
+                          ((period - 1) // 2, period // 2),
+                          (0, 0)))
+    x_pad = np.pad(x, pad_width, mode='edge')
+    trend = sliding_window_view(x_pad, period, axis=1).mean(axis=3)
+    seasonal = x - trend
     return trend, seasonal
 
 
@@ -35,7 +30,6 @@ def differential_decomposition(x):
         trend (numpy.ndarray): Trend component
         seasonal (numpy.ndarray): Seasonal component
     """
-    seasonal = np.diff(x, axis=1)
-    trend = x[:, 1:] - seasonal
+    seasonal = np.pad(np.diff(x, axis=1), (1, 0), mode='edge')
+    trend = x - seasonal
     return trend, seasonal
-
